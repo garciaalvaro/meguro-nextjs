21522cb8d8838801877d8fbee1c3229d1748079d
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-06-28 21:55:34 +0200

    Updated layout styles

--- components/Column/Column.tsx
+++ components/Column/Column.tsx
@@ -3,5 +3,9 @@ import React, { FunctionComponent } from "react";
 import styles from "./Column.styl";
 
 export const Column: FunctionComponent = props => {
-	return <div className={styles.container}>{props.children}</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.content}>{props.children}</div>
+		</div>
+	);
 };
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -14,13 +14,13 @@ export const Header: FunctionComponent<Props> = props => {
 
 	return (
 		<div className={styles.container}>
-			<h1>
+			<h1 className={styles.title}>
 				<Link href={href === "/" ? "/" : "/[slug]"} as={href}>
 					<a>{title}</a>
 				</Link>
 			</h1>
 
-			{subtitle && <h4>{subtitle}</h4>}
+			{subtitle && <h4 className={styles.subtitle}>{subtitle}</h4>}
 		</div>
 	);
 };
