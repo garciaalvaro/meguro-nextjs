c5d6e5631c8af90acb2e12c8997070650ff1ab20
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-07-13 22:00:50 +0200

    Fixed entry not having mobile version check

--- utils/getEntries.ts
+++ utils/getEntries.ts
@@ -1,5 +1,5 @@
 import matter from "gray-matter";
-import { readdirSync, readFileSync } from "fs";
+import { readdirSync, readFileSync, existsSync } from "fs";
 
 export const getEntries = (directory?: string): Entry[] => {
 	if (!directory) {
@@ -11,16 +11,18 @@ export const getEntries = (directory?: string): Entry[] => {
 	const entries = slugs.reduce<Entry[]>((acc, slug) => {
 		const entry = readFileSync(`${directory}/${slug}/index.md`, "utf8");
 
-		const entry_mobile = readFileSync(
-			`${directory}/${slug}/index.mobile.md`,
-			"utf8"
-		);
-
 		const { data: frontmatter, content } = matter(entry);
 
 		let frontmatter_mobile: Entry["frontmatter_mobile"] = {};
 
-		if (entry_mobile) {
+		const has_mobile = existsSync(`${directory}/${slug}/index.mobile.md`);
+
+		if (has_mobile) {
+			const entry_mobile = readFileSync(
+				`${directory}/${slug}/index.mobile.md`,
+				"utf8"
+			);
+
 			frontmatter_mobile = matter(entry_mobile)
 				.data as Entry["frontmatter_mobile"];
 		}
