9339bc0bf730ba56cbdce547c4f48d32687e0273
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-11-28 21:28:44 +0100

    Fixed eslint comment

--- context/Context.ts
+++ context/Context.ts
@@ -5,11 +5,11 @@ export const initial_props: ContextProps = {
 	file_path: "",
 	pages: [],
 
-	// eslint-disable-next-line
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	setMdIsLoading: () => {},
 	md_is_loading: false,
 
-	// eslint-disable-next-line
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	setActiveUrlPath: () => {},
 	active_url_path: "",
 };
