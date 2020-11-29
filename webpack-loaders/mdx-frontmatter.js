ea958ded5e84e7191abdcea338ba3a244e109399
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-11-29 22:49:07 +0100

    Removed path no longer used in frontmatter

--- webpack-loaders/mdx-frontmatter.js
+++ webpack-loaders/mdx-frontmatter.js
@@ -1,24 +1,12 @@
 const matter = require("gray-matter");
-const path = require("path");
 
 module.exports = function (src) {
 	const { content, data: frontmatter } = matter(src);
 
 	if (Object.keys(frontmatter).length) {
-		const file_path = path.relative(
-			path.resolve(__dirname, "../components/Image"),
-			this.context
-		);
-
-		const frontmatter_updated = JSON.stringify({
-			...frontmatter,
-			path: file_path.replace(/^\.\.\/\.\.\/content(-demo)?\//, ""),
-		});
-
-		const src_updated =
-			`export const frontmatter = ${frontmatter_updated};` +
-			"\n" +
-			content;
+		const src_updated = `export const frontmatter = ${JSON.stringify(
+			frontmatter
+		)};\n${content}`;
 
 		return src_updated;
 	}
