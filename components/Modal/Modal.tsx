f33c2f89ce0795b7bbc7785fa4d9bcd8687bf93a
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-12-13 20:00:59 +0100

    Fixed modal image so the container is calculated correctly in portrait and landscape

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,9 +1,16 @@
-import React, { Fragment, FunctionComponent, useState } from "react";
+import React, {
+	Fragment,
+	FunctionComponent,
+	useState,
+	useEffect,
+	useRef,
+} from "react";
 import { createPortal } from "react-dom";
 
-import styles from "./Modal.styl";
-import { Image } from "../Image";
+import { Image, ResponsiveLoader } from "../Image";
+import { useWindowSize } from "@utils/useWindowSize";
 import { Navigation } from "./Navigation";
+import styles from "./Modal.styl";
 
 export interface ModalProps {
 	data: string[];
@@ -16,6 +23,14 @@ export const ModalContent: FunctionComponent<ModalProps> = props => {
 
 	const [direction, setDirection] = useState<"left" | "right" | null>(null);
 
+	const [image_data, setImageData] = useState<ResponsiveLoader | null>(null);
+
+	const $content = useRef<HTMLDivElement | null>(null);
+
+	const [image_style, setImageStyle] = useState({});
+
+	const { window_width, window_height } = useWindowSize();
+
 	const [image_index, setImageIndex] = useState(
 		(() => {
 			const index = data.findIndex(src => src === initial_src);
@@ -23,6 +38,24 @@ export const ModalContent: FunctionComponent<ModalProps> = props => {
 			return index < 0 ? 0 : index;
 		})()
 	);
+
+	useEffect(() => {
+		if (!image_data || !$content.current) return;
+
+		const container_ratio =
+			$content.current.clientWidth / $content.current.clientHeight;
+
+		if (container_ratio > image_data.ratio) {
+			setImageStyle({
+				height: "100%",
+				width: 0,
+				paddingLeft: `${100 * (image_data.ratio / container_ratio)}%`,
+				paddingBottom: undefined,
+			});
+		} else {
+			setImageStyle({});
+		}
+	}, [image_data, window_width, window_height]);
 
 	const goLeft = () => {
 		if (direction) return;
@@ -65,17 +98,22 @@ export const ModalContent: FunctionComponent<ModalProps> = props => {
 				goRight={goRight}
 			/>
 
-			<Image
-				key={image_src}
-				sizes="100vw"
-				className_container={[
-					styles.image_container,
-					...(direction ? [styles[`direction_${direction}`]] : []),
-				].join(" ")}
-				src={image_src}
-				set_padding_bottom={false}
-				onAnimationEnd={() => setDirection(null)}
-			/>
+			<div ref={$content} className={styles.content}>
+				<Image
+					key={image_src}
+					setImageData={setImageData}
+					sizes="100vw"
+					className_container={[
+						styles.image_container,
+						...(direction
+							? [styles[`direction_${direction}`]]
+							: []),
+					].join(" ")}
+					src={image_src}
+					style_image={image_style}
+					onAnimationEnd={() => setDirection(null)}
+				/>
+			</div>
 		</div>
 	);
 };
