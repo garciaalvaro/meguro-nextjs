a48a32ac86e87b8511e3359ba08cabfe8858c11d
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-04-03 11:44:09 +0200

    Use explicit import of types

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import Document, {
-	Html,
-	Head,
-	Main,
-	NextScript,
-	DocumentContext,
-} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import type { DocumentContext } from "next/document";
 
 class MyDocument extends Document {
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
