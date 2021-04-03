cbd11d0a0beb91194f4153ec5531297a8e6b9e7e
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-04-03 11:52:32 +0200

    Fixed return type of functions

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import type { DocumentContext } from "next/document";
+import type { DocumentContext, DocumentInitialProps } from "next/document";
 
 class MyDocument extends Document {
-	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-	static async getInitialProps(ctx: DocumentContext) {
+	static async getInitialProps(
+		ctx: DocumentContext
+	): Promise<DocumentInitialProps> {
 		const initialProps = await Document.getInitialProps(ctx);
 		return { ...initialProps };
 	}
 
-	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-	render() {
+	render(): JSX.Element {
 		return (
 			<Html lang={process.env.lang}>
 				<Head />
