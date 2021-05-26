2a559ed611ae7e311e57527de5751185d1a0ecfd
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-05-26 23:44:06 +0200

    Removed is_resizing styles in Sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from "react";
 import type { FunctionComponent } from "react";
 import Scrollbar from "react-scrollbars-custom";
 
-import { useIsMobile, useIsFirstRender, useWindowSize } from "@hooks";
+import { useIsMobile, useIsFirstRender } from "@hooks";
 import { className } from "@utils";
 import { List } from "./List";
 import styles from "./Sidebar.styl";
@@ -14,7 +14,6 @@ export const Sidebar: FunctionComponent = () => {
 	const is_mobile = useIsMobile(600);
 	const is_first_render = useIsFirstRender();
 	const $scroller = useRef<Scrollbar | null>(null);
-	const { is_resizing } = useWindowSize();
 
 	const toggle = () => {
 		if (is_opening || is_closing) return;
@@ -36,7 +35,6 @@ export const Sidebar: FunctionComponent = () => {
 				[styles.is_closed]: !is_open,
 				[styles.is_opening]: is_opening,
 				[styles.is_closing]: is_closing,
-				[styles.is_resizing]: is_resizing,
 			})}
 			onClick={toggle}
 			aria-expanded={is_open}
