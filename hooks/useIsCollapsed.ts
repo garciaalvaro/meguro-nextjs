18b3f522e1c3d661a62cee9c7c4bdf09e2dc5ddd
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-05-29 15:58:47 +0200

    Fixed useIsCollapsed syntax

--- hooks/useIsCollapsed.ts
+++ hooks/useIsCollapsed.ts
@@ -2,15 +2,13 @@ import { useState, useEffect } from "react";
 
 import { useWindowSize } from "./useWindowSize";
 
-export const useIsCollapsed = (breakpoint?: number): boolean => {
+export const useIsCollapsed = (breakpoint = 0): boolean => {
 	const { window_width } = useWindowSize();
 
-	const [is_collapsed, setIsCollapsed] = useState(
-		window_width < (breakpoint || 0)
-	);
+	const [is_collapsed, setIsCollapsed] = useState(window_width < breakpoint);
 
 	useEffect(() => {
-		setIsCollapsed(window_width < (breakpoint || 0));
+		setIsCollapsed(window_width < breakpoint);
 	}, [window_width, breakpoint]);
 
 	return is_collapsed;
